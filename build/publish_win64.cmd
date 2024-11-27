cb35f628492710039a42865cb59e4754b6a3efb6
ema <mccoy39082@163.com>
2024-11-27 22:38:33 +0800

    chore: change win64 build script

--- build/publish_win64.cmd
+++ build/publish_win64.cmd
@@ -6,13 +6,9 @@ cd /d %~dp0
 
 del /s /q ..\src\TiktokLiveRec.Windows\bin\x64\Release\net9.0-windows10.0.26100.0\publish\win-x64\TiktokLiveRec.exe
 dotnet publish ..\src\TiktokLiveRec.Windows\TiktokLiveRec.Windows.csproj -c Release -p:PublishProfile=FolderProfile
-rd /s /q ..\src\TiktokLiveRec.Windows\bin\x64\Release\net9.0-windows10.0.26100.0\publish\win-x64\..\TiktokLiveRec\
-mkdir ..\src\TiktokLiveRec.Windows\bin\x64\Release\net9.0-windows10.0.26100.0\publish\win-x64\..\TiktokLiveRec\
 rd /s /q ..\src\TiktokLiveRec.Windows\bin\x64\Release\net9.0-windows10.0.26100.0\publish\win-x64\downloads\
-xcopy /s /e /y ..\src\TiktokLiveRec.Windows\bin\x64\Release\net9.0-windows10.0.26100.0\publish\win-x64\* ..\src\TiktokLiveRec.Windows\bin\x64\Release\net9.0-windows10.0.26100.0\publish\win-x64\..\TiktokLiveRec\
 del /s /q publish.7z
-7z a publish.7z ..\src\TiktokLiveRec.Windows\bin\x64\Release\net9.0-windows10.0.26100.0\publish\win-x64\..\TiktokLiveRec\ -t7z -mx=5 -mf=BCJ2 -r -y
-rd /s /q ..\src\TiktokLiveRec.Windows\bin\x64\Release\net9.0-windows10.0.26100.0\publish\win-x64\..\TiktokLiveRec\
+7z a publish.7z ..\src\TiktokLiveRec.Windows\bin\x64\Release\net9.0-windows10.0.26100.0\publish\win-x64\* -t7z -mx=5 -mf=BCJ2 -r -y
 for /f "usebackq delims=" %%i in (`powershell -NoLogo -NoProfile -Command "Get-Content '..\src\TiktokLiveRec.Windows\TiktokLiveRec.Windows.csproj' | Select-String -Pattern '<AssemblyVersion>(.*?)</AssemblyVersion>' | ForEach-Object { $_.Matches.Groups[1].Value }"`) do @set version=%%i
 del /s /q TiktokLiveRec_v%version%_win64.7z
 "C:\Program Files\MicaSetup\makemica" micasetup.json
