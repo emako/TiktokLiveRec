7fd684f10b8440a49081ad3f283b35e0ccdef07d
ema <mccoy39082@163.com>
2024-12-02 01:09:44 +0800

    chore: build win64 copy bin files

--- build/publish_win64.cmd
+++ build/publish_win64.cmd
@@ -7,6 +7,8 @@ cd /d %~dp0
 del /s /q ..\src\TiktokLiveRec.Windows\bin\x64\Release\net9.0-windows10.0.26100.0\publish\win-x64\TiktokLiveRec.exe
 dotnet publish ..\src\TiktokLiveRec.Windows\TiktokLiveRec.Windows.csproj -c Release -p:PublishProfile=FolderProfile
 rd /s /q ..\src\TiktokLiveRec.Windows\bin\x64\Release\net9.0-windows10.0.26100.0\publish\win-x64\downloads\
+copy ffmpeg.exe ..\src\TiktokLiveRec.Windows\bin\x64\Release\net9.0-windows10.0.26100.0\publish\win-x64\
+copy ffplay.exe ..\src\TiktokLiveRec.Windows\bin\x64\Release\net9.0-windows10.0.26100.0\publish\win-x64\
 del /s /q publish.7z
 7z a publish.7z ..\src\TiktokLiveRec.Windows\bin\x64\Release\net9.0-windows10.0.26100.0\publish\win-x64\* -t7z -mx=5 -mf=BCJ2 -r -y
 for /f "usebackq delims=" %%i in (`powershell -NoLogo -NoProfile -Command "Get-Content '..\src\TiktokLiveRec.Windows\TiktokLiveRec.Windows.csproj' | Select-String -Pattern '<AssemblyVersion>(.*?)</AssemblyVersion>' | ForEach-Object { $_.Matches.Groups[1].Value }"`) do @set version=%%i
