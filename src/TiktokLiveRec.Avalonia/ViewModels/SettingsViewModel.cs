6d5f9de56d59636b64db3c90da2ec33e9290c283
ema <mccoy39082@163.com>
2025-04-13 22:22:40 +0800

    feat: toast control

--- src/TiktokLiveRec.Avalonia/ViewModels/SettingsViewModel.cs
+++ src/TiktokLiveRec.Avalonia/ViewModels/SettingsViewModel.cs
@@ -1,9 +1,13 @@
 using Avalonia;
 using Avalonia.Styling;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using Fischless.Configuration;
 using FluentAvaloniaUI.Violeta.Platform;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
+using TiktokLiveRec.Extensions;
 
 namespace TiktokLiveRec.ViewModels;
 
@@ -80,5 +84,34 @@ public partial class SettingsViewModel : ObservableObject
             _ => string.Empty,
         });
         ConfigurationManager.Save();
+    }
+
+    [ObservableProperty]
+    private bool isUseStatusTray = Configurations.IsUseStatusTray.Get();
+
+    partial void OnIsUseStatusTrayChanged(bool value)
+    {
+        Configurations.IsUseStatusTray.Set(value);
+        ConfigurationManager.Save();
+        TrayIconManager.GetInstance().UpdateTrayIcon();
+    }
+
+    [SuppressMessage("Interoperability", "CA1416:Validate platform compatibility")]
+    [SuppressMessage("CodeQuality", "IDE0079:Remove unnecessary suppression")]
+    [Description("Only used for Windows")]
+    [RelayCommand]
+    private void CreateDesktopShortcut()
+    {
+        if (Environment.OSVersion.Platform == PlatformID.Win32NT)
+        {
+            ShortcutHelper.CreateShortcutOnDesktop(
+                shortcutName: "TiktokLiveRec",
+                targetPath: Path.Combine(AppDomain.CurrentDomain.BaseDirectory, AppDomain.CurrentDomain.FriendlyName),
+                arguments: null!,
+                description: "Title".Tr(),
+                iconLocation: Path.Combine(AppDomain.CurrentDomain.BaseDirectory, AppDomain.CurrentDomain.FriendlyName + ".exe"));
+
+            Toast.Success("SuccOp".Tr());
+        }
     }
 }
